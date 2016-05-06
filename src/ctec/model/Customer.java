d474d5cadbeb19b43a20f2f6d8ada066d9f9fdc3
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-05-06 08:46:51 -0600

    Added to dish

--- src/ctec/model/Customer.java
+++ src/ctec/model/Customer.java
@@ -20,7 +20,7 @@ public class Customer
 	
 	public String name()
 	{
-		return "";
+		return "Bobby";
 	}
 	
 	public double total()
--- src/ctec/model/Dish.java
+++ src/ctec/model/Dish.java
@@ -1,6 +1,11 @@
 package ctec.model;
 
+import java.awt.Color;
+
 public class Dish
 {
+	private boolean isDirty;
+	private Color blue;
+	private int radius;
 
 }
